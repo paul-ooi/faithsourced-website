d5451376fd258bfaf5696828faba7ec1c4054160
Mark Jenkinson <mark@togglemedia.com>
2019-04-12 11:55:22 -0400

    add social links to footer
    Signed-off-by: Mark Jenkinson <mark@togglemedia.com>

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,13 @@ import PropTypes from 'prop-types'
 
 const Footer = (props) => (
     <footer id="footer" style={props.timeout ? {display: 'none'} : {}}>
-        <p className="copyright">&copy; 2019 Faith Sourced Software Foundation.</p>
+		<ul className="icons">
+			<li><a href="https://twitter.com/faithsourced" target="_blank" className="icon fa-twitter"><span className="label">Twitter</span></a></li>
+            <li><a href="https://facebook.com/faithsourced" target="_blank" className="icon fa-facebook"><span className="label">Facebook</span></a></li>
+            <li><a href="https://instagram.com/faithsourced" target="_blank" className="icon fa-instagram"><span className="label">Instagram</span></a></li>
+            <li><a href="https://github.com/faithsourced" target="_blank" className="icon fa-github"><span className="label">GitHub</span></a></li>
+		</ul>
+		<p className="copyright">&copy; 2019 Faith Sourced Software Foundation.</p>
     </footer>
 )
 
