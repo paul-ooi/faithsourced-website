204444e3e6eb7ab9af4f7baaeb910190be2bfbe1
Mark Jenkinson <mark@togglemedia.com>
2019-05-02 08:52:24 -0400

    fix meta description and keywords

--- src/components/layout.js
+++ src/components/layout.js
@@ -41,8 +41,8 @@ const Layout = ({ children, location }) => {
           <Helmet
             title={data.site.siteMetadata.title}
             meta={[
-              { name: 'description', content: 'Sample' },
-              { name: 'keywords', content: 'sample, something' },
+              { name: 'description', content: 'Faith Sourced is a community of software developers using their skills for Jesus. We call it "Software as a Service" where Christ is the center of all we are and do. Faith Sourced is our contribution to the cause of Christ, and His church. Faith Sourced is our chance to use our uniquely technical skills for purposes that are eternal, united under one banner: the gospel of Jesus Christ. The software we build is freely available to the body of Christ, for His glory.' },
+              { name: 'keywords', content: 'software,developer,code,open source,faith,christian,jesus,church,meetups,toronto,canada' },
             ]}
           >
             <html lang="en" />
