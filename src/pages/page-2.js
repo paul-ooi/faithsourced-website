ae56b0c78300236159aef4861b21f0594fc0d4ef
Mark Jenkinson <mark@togglemedia.com>
2019-05-02 11:12:14 -0400

    fix page-2

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -3,12 +3,18 @@ import { Link } from 'gatsby'
 
 import Layout from '../components/layout'
 
-const SecondPage = () => (
-  <Layout>
-    <h1>Hi from the second page</h1>
-    <p>Welcome to page 2</p>
-    <Link to="/">Go back to the homepage</Link>
-  </Layout>
-)
-
+class ThanksPage extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+	render() {
+		return (
+		  <Layout location={this.props.location}>
+			<h1>Hi from the second page</h1>
+			<p>Welcome to page 2</p>
+			<Link to="/">Go back to the homepage</Link>
+		  </Layout>
+		)
+	}
+}
 export default SecondPage
