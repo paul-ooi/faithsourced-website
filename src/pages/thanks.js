7f0ffe90593f43196ae1ed826945f4e1b6a8ea52
Mark Jenkinson <mark@togglemedia.com>
2019-05-09 09:37:42 -0400

    moved page wrapper to layout.js

--- src/pages/thanks.js
+++ src/pages/thanks.js
@@ -2,34 +2,20 @@ import React from 'react'
 import { Link } from 'gatsby'
 
 import Layout from '../components/layout'
-import Footer from '../components/Footer'
 
 class ThanksPage extends React.Component {
-  constructor(props) {
-    super(props)
-  }
+	constructor(props) {
+		super(props)
+	}
 	render() {
 		return (
-			<Layout location={this.props.location}>
-				<div>
-					<div id="wrapper">
-						<div id="main" style={{display:'flex'}}>
-							<header id="header">
-								<div className="logo" alt="Faith Sourced Logo"></div>
-								<div className="content">
-									<div className="inner">
-										<h1>Thank you!</h1>
-										<p className="align-center">We are excited to hear about your interest in the Faith Sourced project. Expect to hear back from us within 1-2 business days, if not sooner.</p>
-										<p>While you're waiting, we would love it if you took a few moments to follow us on <a href="https://twitter.com/faithsourced/" target="_blank">Twitter</a>, like us on <a href="https://facebook.com/faithsourced/" target="_blank">Facebook</a>, and subscribe to our <a href="https://youtube.com/channel/faithsourced/" target="_blank">YouTube</a> channel.</p>
-										<Link to="/">Home</Link>
-									</div>
-								</div>
-							</header>
-						</div>
-						<Footer/>
-					</div>
-					<div id="bg"></div>
-				</div>
+			<Layout location={this.props.location}>				
+				<div className="logo faithsourced" alt="Faith Sourced Logo"></div>
+				<h1 className="align-center">Thank you!</h1>
+				<p className="align-center">We are excited to hear about your interest in the Faith Sourced project.</p>
+				<p className="align-center">Expect to hear back from us within 1-2 business days, if not sooner.</p>
+				<p className="align-center">While you're waiting, we would love it if you took a few moments to follow us on <a href="https://twitter.com/faithsourced/" target="_blank">Twitter</a>, like us on <a href="https://facebook.com/faithsourced/" target="_blank">Facebook</a>, and subscribe to our <a href="https://youtube.com/channel/faithsourced/" target="_blank">YouTube</a> channel.</p>
+				<p  className="align-center"><Link to="/">Back</Link></p>
 			</Layout>
 		)
 	}
