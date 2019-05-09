dda346bbffd3569e83e9d973b1c2a9262f2bf6b5
Mark Jenkinson <mark@togglemedia.com>
2019-05-09 09:25:38 -0400

    updated 404 text

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import { Link } from 'gatsby'
+
 import Layout from '../components/layout'
 
 class NotFoundPage extends React.Component {
@@ -8,8 +10,10 @@ class NotFoundPage extends React.Component {
 	render() {
 		return (
 		  <Layout location={this.props.location}>
-			<h1>NOT FOUND</h1>
-			<p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+			<div className="logo faithsourced" alt="Faith Sourced Logo"></div>
+			<h1 className="align-center">Whoops!</h1>
+			<p className="align-center">The page you are looking for has either moved, or it never existed.</p>
+			<p className="align-center"><Link to="/">Home</Link></p>
 		  </Layout>
 		)
 	}
