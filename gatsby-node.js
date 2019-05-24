50bc18cfc97b3ab51064b43c2b11235a0bbb420c
Mark Jenkinson <mark@togglemedia.com>
2019-05-24 11:57:02 -0400

    graphql query for blog posts and createPage

--- gatsby-node.js
+++ gatsby-node.js
@@ -5,59 +5,81 @@
  */
 
 const path = require("path")
-const { createFilePath, createFileNode } = require(`gatsby-source-filesystem`)
+const { createFilePath, createFileNode, createRemoteFileNode} = require(`gatsby-source-filesystem`)
 
 exports.createPages = ({ actions, graphql }) => {
     const { createPage } = actions
-
-    const blogPostTemplate = path.resolve(`src/templates/blog-post.js`)
-
     return new Promise((resolve, reject) => {
+		resolve(graphql(`{
+				mdBlogs: allMarkdownRemark(
+					sort: { order: DESC, fields: [frontmatter___date] }
+					limit: 1000
+				) {
+					edges {
+						node {
+							fields {
+								slug
+							}
+						}
+					}
+				}
+				jsonBlogs: allThirdPartyPosts(
+					sort: { order: DESC, fields: [date] }
+					limit: 1000
+				) {
+					edges {
+						node {
+							title
+							slug
+						}
+					}		
+				}
+			}
+			`).then(result => {
+				if (result.errors) {
+					console.log(result.errors)
+					return reject(result.errors)
+				}
 
-        resolve(graphql(`
-    {
-      allMarkdownRemark(
-        sort: { order: DESC, fields: [frontmatter___date] }
-        limit: 1000
-      ) {
-        edges {
-          node {
-              fields{
-                  slug
-              }
-            frontmatter {
-              title
-            }
-          }
-        }
-      }
-    }
-  `).then(result => {
-                if (result.errors) {
-                    console.log(result.errors)
-                    return reject(result.errors)
-                }
+				const mdBlogTemplate = path.resolve('./src/templates/blog-post.js')
 
-                const blogTemplate = path.resolve('./src/templates/blog-post.js');
-
-                result.data.allMarkdownRemark.edges.forEach(({ node }) => {
-                    createPage({
-                        path: node.fields.slug,
-                        component: blogTemplate,
-                        context: {
-                            slug: node.fields.slug,
-                        }, // additional data can be passed via context
-                    })
-                })
-                return
-            })
-        )
-    })
+				result.data.mdBlogs.edges.forEach(({ node }) => {
+					createPage({
+						path: node.fields.slug,
+						component: mdBlogTemplate,
+						context: {
+							slug: node.fields.slug,
+						},
+					})
+				})
+				
+				const jsonBlogTemplate = path.resolve('./src/templates/blog-post-json.js')
+				const posts = result.data.jsonBlogs.edges
+				
+				posts.forEach(({ node }, index) => {
+					const prev = index === posts.length - 1 ? null : posts[index + 1].node
+					const next = index === 0 ? null : posts[index - 1].node
+					
+					createPage({
+						path: `${node.slug}`,
+						component: jsonBlogTemplate,
+						context: {
+							slug: node.slug,
+							prev,
+							next,
+						},
+					})
+				})
+				return 
+			})
+		)}
+	)	
 }
 
 
 exports.onCreateNode = ({ node, getNode, actions }) => {
     const { createNodeField } = actions
+    
     if (node.internal.type === `MarkdownRemark`) {
         const slug = createFilePath({ node, getNode, basePath: `pages` })
         createNodeField({
@@ -65,6 +87,5 @@ exports.onCreateNode = ({ node, getNode, actions }) => {
             name: `slug`,
             value: `blog${slug}`,
         })
-
-    }
+	}
 }
